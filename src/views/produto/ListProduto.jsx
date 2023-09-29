bdde85fb99906bc1594b2d4083384c3080912099
JaimeFcls <119618310+JaimeFcls@users.noreply.github.com>
2023-09-29 16:19:34 -0300

    upt

--- src/views/produto/ListProduto.jsx
+++ src/views/produto/ListProduto.jsx
@@ -1,103 +1,102 @@
-import axios from 'axios';
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
-import MenuSistema from '../../MenuSistema';
+import { Button, Container, Divider, Icon, Table } from "semantic-ui-react";
+import MenuSistema from "../../MenuSistema";
 
-export default function ListProduto () {
+export default function ListProduto() {
+  const [lista, setLista] = useState([]);
 
-   const [lista, setLista] = useState([]);
+  useEffect(() => {
+    carregarLista();
+  }, []);
 
-   useEffect(() => {
-       carregarLista();
-   }, [])
-
-   function carregarLista() {
-
-       axios.get("http://localhost:8082/api/produto")
-       .then((response) => {
-           setLista(response.data)
-           
-       })
-       .catch((error) => {
+  function carregarLista() {
+    axios
+      .get("http://localhost:8082/api/produto")
+      .then((response) => {
+        setLista(response.data);
+      })
+      .catch((error) => {
         // Handle the error here
-        console.error('Axios Error:', error); 
-   })};
-   
-return(
+        console.error("Axios Error:", error);
+      });
+  }
+
+  return (
     <div>
-        <MenuSistema />
-        <div style={{marginTop: '3%'}}>
+      <MenuSistema />
+      <div style={{ marginTop: "3%" }}>
+        <Container textAlign="justified">
+          <h2> Produtos </h2>
+          <Divider />
 
-            <Container textAlign='justified' >
+          <div style={{ marginTop: "4%" }}>
+            <Button
+              label="Novo"
+              circular
+              color="orange"
+              icon="clipboard outline"
+              floated="right"
+              as={Link}
+              to="/form-produto"
+            />
+            <br />
+            <br />
+            <br />
 
-                <h2> Produtos </h2>
-                <Divider />
+            <Table color="orange" sortable celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Titulo</Table.HeaderCell>
+                  <Table.HeaderCell>Descrição</Table.HeaderCell>
+                  <Table.HeaderCell>Valor Unitario</Table.HeaderCell>
+                  <Table.HeaderCell textAlign="center">Ações</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
 
-                <div style={{marginTop: '4%'}}>
-                    <Button
-                        label='Novo'
+              <Table.Body>
+                {lista.map((produto) => (
+                  <Table.Row key={produto.id}>
+                    <Table.Cell>{produto.titulo}</Table.Cell>
+                    <Table.Cell>{produto.descricao}</Table.Cell>
+                    <Table.Cell>{"R$ " + produto.valorUnitario}</Table.Cell>
+
+                    <Table.Cell textAlign="center">
+                      <Button
+                        inverted
                         circular
-                        color='orange'
-                        icon='clipboard outline'
-                        floated='right'
-                        as={Link}
-                        to='/form-produto'
-                    />
-             <br/><br/><br/>
-                  
-                  <Table color='orange' sortable celled>
-
-                      <Table.Header>
-                          <Table.Row>
-                              <Table.HeaderCell>Titulo</Table.HeaderCell>
-                              <Table.HeaderCell>Descrição</Table.HeaderCell>
-                              <Table.HeaderCell>Valor Unitario</Table.HeaderCell>
-                              <Table.HeaderCell textAlign='center'>Ações</Table.HeaderCell>
-                          </Table.Row>
-                      </Table.Header>
-                 
-                      <Table.Body>
-
-                          { lista.map(produto => (
-
-                              <Table.Row key={produto.id}>
-                                  <Table.Cell>{produto.titulo}</Table.Cell>
-                                  <Table.Cell>{produto.descricao}</Table.Cell>
-                                  <Table.Cell>{"R$ "+produto.valorUnitario}</Table.Cell>
-                                  
-                                  
-                                  <Table.Cell textAlign='center'>
-
-                                      <Button
-                                          inverted
-                                          circular
-                                          color='green'
-                                          title='Clique aqui para editar os dados deste produto'
-                                          icon>
-                                               <Icon name='edit' />
-                                      </Button> &nbsp;
-                                      <Button
-                                               inverted
-                                               circular
-                                               color='red'
-                                               title='Clique aqui para remover este produto'
-                                               icon>
-                                                   <Icon name='trash' />
-                                           </Button>
-
-                                       </Table.Cell>
-                                   </Table.Row>
-                               ))}
-
-                           </Table.Body>
-                       </Table>
-                   </div>
-               </Container>
-           </div>
-
-       </div>
-   )
+                        color="green"
+                        title="Clique aqui para editar os dados deste cliente"
+                        icon
+                      >
+                        <Link
+                          to="/form-produto"
+                          state={{ id: produto.id }}
+                          style={{ color: "green" }}
+                        >
+                          {" "}
+                          <Icon name="edit" />{" "}
+                        </Link>
+                      </Button>{" "}
+                      &nbsp;
+                      <Button
+                        inverted
+                        circular
+                        color="red"
+                        title="Clique aqui para remover este produto"
+                        icon
+                      >
+                        <Icon name="trash" />
+                      </Button>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 }
-      
-            
--- src/views/produto/import axios from 'axios';.ts
+++ src/views/produto/import axios from 'axios';.ts
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
-export default function ListEntregador () {
+export default function ListProduto () {
 
    const [lista, setLista] = useState([]);
 
@@ -14,20 +14,16 @@ export default function ListEntregador () {
 
    function carregarLista() {
 
-       axios.get("http://localhost:8082/api/entregador")
+       axios.get("http://localhost:8082/api/produto")
        .then((response) => {
            setLista(response.data)
+           
        })
-   }
-   function formatarData(dataParam) {
-
-    if (dataParam === null || dataParam === '' || dataParam === undefined) {
-        return ''
-    }
-
-    let arrayData = dataParam.split('-');
-    return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
-}
+       .catch((error) => {
+        // Handle the error here
+        console.error('Axios Error:', error); 
+   })};
+   
 return(
     <div>
         <MenuSistema />
@@ -35,7 +31,7 @@ return(
 
             <Container textAlign='justified' >
 
-                <h2> Entregadores </h2>
+                <h2> Produtos </h2>
                 <Divider />
 
                 <div style={{marginTop: '4%'}}>
@@ -46,7 +42,7 @@ return(
                         icon='clipboard outline'
                         floated='right'
                         as={Link}
-                        to='/form-entregador'
+                        to='/form-produto'
                     />
              <br/><br/><br/>
                   
@@ -54,44 +50,38 @@ return(
 
                       <Table.Header>
                           <Table.Row>
-                              <Table.HeaderCell>Nome</Table.HeaderCell>
-                              <Table.HeaderCell>CPF</Table.HeaderCell>
-                              <Table.HeaderCell>Data de Nascimento</Table.HeaderCell>
-                              <Table.HeaderCell>Fone Celular</Table.HeaderCell>
-                              <Table.HeaderCell>UF</Table.HeaderCell>
-                              
-                              
-                              
+                              <Table.HeaderCell>Titulo</Table.HeaderCell>
+                              <Table.HeaderCell>Descrição</Table.HeaderCell>
+                              <Table.HeaderCell>Valor Unitario</Table.HeaderCell>
                               <Table.HeaderCell textAlign='center'>Ações</Table.HeaderCell>
                           </Table.Row>
                       </Table.Header>
                  
                       <Table.Body>
 
-                          { lista.map(entregador => (
+                          { lista.map(produto => (
 
-                              <Table.Row key={entregador.id}>
-                                  <Table.Cell>{entregador.nome}</Table.Cell>
-                                  <Table.Cell>{entregador.cpf}</Table.Cell>
-                                  <Table.Cell>{entregador.dataNascimento}</Table.Cell>
-                                  <Table.Cell>{entregador.foneCelular}</Table.Cell>
-                                  <Table.Cell>{entregador.enderecoUf}</Table.Cell>
+                              <Table.Row key={produto.id}>
+                                  <Table.Cell>{produto.titulo}</Table.Cell>
+                                  <Table.Cell>{produto.descricao}</Table.Cell>
+                                  <Table.Cell>{produto.valorUnitario}</Table.Cell>
+                                  
+                                  
                                   <Table.Cell textAlign='center'>
 
-                                  <Button
-         inverted
-         circular
-         color='green'
-         title='Clique aqui para editar os dados deste Entregador'
-         icon>
-            <Link to="/form-entregador" state={{id: entregador.id}} style={{color: 'green'}}> <Icon name='edit' /> </Link>
-      </Button> &nbsp;
-
+                                      <Button
+                                          inverted
+                                          circular
+                                          color='green'
+                                          title='Clique aqui para editar os dados deste produto'
+                                          icon>
+                                               <Icon name='edit' />
+                                      </Button> &nbsp;
                                       <Button
                                                inverted
                                                circular
                                                color='red'
-                                               title='Clique aqui para remover este entregador'
+                                               title='Clique aqui para remover este produto'
                                                icon>
                                                    <Icon name='trash' />
                                            </Button>
